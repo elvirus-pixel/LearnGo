124fc611b74d13d131010555346894e25614922c
elvirus <276036461+elvirus-pixel@users.noreply.github.com>
2026-05-08 12:27:51 -0400

    lessons 33-37

---
+++ 33 - select.go
@@ -0,0 +1,45 @@
+// Go’s select lets you wait on multiple channel operations. Combining goroutines and channels with select is a powerful feature of Go.
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    // For our example we’ll select across two channels.
+    c1 := make(chan string)
+    c2 := make(chan string)
+
+    // Each channel will receive a value after some amount of time, to simulate e.g. blocking RPC operations executing in concurrent goroutines.
+    go func() {
+        time.Sleep(1 * time.Second)
+        c1 <- "one"
+    }()
+    go func() {
+        time.Sleep(2 * time.Second)
+        c2 <- "two"
+    }()
+
+    // We’ll use `select` to await both of these values simultaneously, printing each one as it arrives.
+    for range 2 {
+        select {
+        case msg1 := <-c1:
+            fmt.Println("received", msg1)
+        case msg2 := <-c2:
+            fmt.Println("received", msg2)
+        }
+    }
+}
+// We receive the values "one" and then "two" as expected.
+// Note that the total execution time is only ~2 seconds since both the 1 and 2 second Sleeps execute concurrently.
+
+/*
+$ time go run '33 - select.go' 
+received one
+received two
+
+real    0m2.212s
+user    0m0.268s
+sys     0m0.109s
+*/
